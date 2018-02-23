1be50bd94ac50d4cefee9ed64929bae91abfda56
istark19 <33226547+istark19@users.noreply.github.com>
2018-02-23 11:04:39 -0600

    Update 13-1-2.py

--- 13-1-2.py
+++ 13-1-2.py
@@ -5,13 +5,13 @@ import time
 current_time = time.time()
 
 while True:
-  while current_time % 3 != 0:
+  while int(current_time) % 3 != 0:
     import RoboPiLib as RPL
     import setup
     RPL.servoWrite(1,500)
     RPL.servoWrite(0,2000)
   
-  while current_time % 3 == 0:
+  while int(current_time) % 3 == 0:
     import RoboPiLib as RPL
     import setup
     RPL.servoWrite(1,0)
